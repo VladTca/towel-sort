baed6caa31aacc0e4d0ea05f2765f15a311c7b7e
VladTca <votsats@gmail.com>
2024-08-09 10:59:28 +0200

    ✨ Add .idea directory to .gitignore and implement towelSort function in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+const towel=[
+    [ 1, 2, 3 ],
+    [ 4, 5, 6 ],
+    [ 7, 8, 9 ],
+]
+
+const towelSort = (matrix) => {
+    return towel.flat().sort((a, b) => a - b)
 }
+
+console.log(towelSort(towel))
+
+// module.exports = function towelSort (matrix) {
+//   return [];
+// }
